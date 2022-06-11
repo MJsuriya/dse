1fa7faf40c06815287f4644c4dfa379a85f0fd83
Jeya Suriya Muthumari <jeya.suriyam@emsibg.com>
2022-06-11 21:10:53 +0530

    Added new package to use scss & playground to test it

--- packages/scss/src/scripts/build.js
+++ packages/scss/src/scripts/build.js
@@ -10,7 +10,7 @@ const getComponents = () => {
   types.forEach((type) => {
     const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
       input: `src/${type}/${file}`,
-      output: `src/lib/${file.slice(0, -4) + "css"}`,
+      output: `lib/${file.slice(0, -4) + "css"}`,
     }));
 
     allComponents = [...allComponents, ...allFiles];
@@ -33,7 +33,7 @@ try {
   Fs.mkdirSync(Path.resolve("lib"));
 } catch (e) {}
 
-compile("src/global.scss", "src/lib/global.css");
+compile("src/global.scss", "lib/global.css");
 
 getComponents().forEach((component) => {
   compile(component.input, component.output);
---
+++ playgrounds/react/src/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@dse/react";
+import "@dse/scss/lib/button.css";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <Button label="Example Button"></Button>,
+  document.querySelector("#root")
+);
