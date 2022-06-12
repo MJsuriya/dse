1372c410b72f6d51792b16a60d7a756dc7226703
Jeya Suriya Muthumari <jeya.suriyam@emsibg.com>
2022-06-12 14:22:51 +0530

    [27, 28]: Added color component and generated dynamic stypes

---
+++ packages/react/lib/foundation/Spacing.js
@@ -0,0 +1,15 @@
+const Spacing = {
+    none: "none",
+    xxxs: "xxxs",
+    xxs: "xxs",
+    xs: "xs",
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "xl",
+    xxl: "xxl",
+    xxxl: "xxxl",
+};
+
+export { Spacing as default };
+//# sourceMappingURL=Spacing.js.map
--- playgrounds/react/src/index.tsx
+++ playgrounds/react/src/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@dse/react";
-import "@dse/scss/lib/button.css";
+import { Color } from "@dse/react";
+import "@dse/scss/lib/Utilities.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <Button label="Example Button"></Button>,
+  <Color hexCode="#000" width="lg" height="lg"></Color>,
   document.querySelector("#root")
 );
